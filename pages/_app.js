927700848fe9108ed91f8b614cf67a8bd903a14c
zeongeofano <zeongeofano@gmail.com>
2026-04-20 01:46:47 +0700

    Update _app.js

--- pages/_app.js
+++ pages/_app.js
@@ -1,4 +1,8 @@
+
 import '../styles/globals.css'
-export default function MyApp({ Component, pageProps }) {
+
+function MyApp({ Component, pageProps }) {
   return <Component {...pageProps} />
 }
+
+export default MyApp
